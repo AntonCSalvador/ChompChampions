e3323f2706046a214a261867973a087aee1c00a6
ZachBrad11 <zach.bradley1218.2002@gmail.com>
2023-08-14 14:31:04 -0400

    Updated champions.yp file

---
+++ champions.py
@@ -0,0 +1,24 @@
+class Champions:
+
+    def __init__(self, speed, hit_box, strength, weight, resistance):
+        self.speed = speed
+        self.default_speed = speed
+        self.hit_box = hit_box
+        self.strength = strength
+        self.default_strength = strength
+        self.weight = weight
+        self.resistance = resistance
+
+    def update_speed(self, speed_multiplier):
+        self.speed = self.speed * speed_multiplier
+
+    def reset_speed(self):
+        self.speed = self.default_speed
+
+    def update_strength(self, strength_multiplier):
+        self.strength = self.strength * strength_multiplier
+
+    def reset_speed(self):
+        self.strength = self.default_strength
+
+    def update_
