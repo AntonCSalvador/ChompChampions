db56ecbb55809614e311547a6a441214629dd1dc
AntonCSalvador <antoncsalvador@Gmail.com>
2023-08-15 01:11:14 -0400

    fixed collision with radius

--- championstest.py
+++ championstest.py
@@ -8,10 +8,10 @@ class Player:
     def move(self, dx, dy):
         self.current_position = (self.current_position[0] + dx, self.current_position[1] + dy)
 
-    def draw():
+    def draw(self):
         print("hi")
 
-    def update():
+    def update(self):
         print("hi")
         # this.position.y += this.velocity
 
@@ -20,13 +20,15 @@ class Player:
 
 class Projectile:
     def __init__(self, x, y, velocity):
-        self.start_position = (x,y)
+        self.start_position = (x, y)
         self.current_position = self.start_position
         self.velocity = velocity
+        self.radius = 10  # Radius for the circle projectile
+        self.rect = pygame.Rect(x - self.radius, y + 20 - self.radius, self.radius * 2, self.radius * 2)
 
-
-    def move(self, dx, dy, velocity):
+    def move(self, dx, dy):
         self.current_position = (self.current_position[0] + dx, self.current_position[1] + dy)
+        self.rect.move_ip(dx, dy)
 
 class Floor:
     def __init__(self, x, y, width, height):
@@ -36,17 +38,17 @@ class Floor:
         pygame.draw.rect(surface, (0, 255, 0), self.rect)  # Green color for the ground
 
 class HealthBar():
-    def __init__(self,x,y,w,h,max_hp):
-        self.x=x
-        self.y=y 
+    def __init__(self, x, y, w, h, max_hp):
+        self.x = x
+        self.y = y 
         self.w = w
         self.h = h
-        self.hp = max_hp #bc you assume it starts at full health 
+        self.hp = max_hp  # Assume it starts at full health 
         self.max_hp = max_hp
 
     def draw(self, surface):
-        #calculate health ratio
-        ratio = self.hp/self.max_hp
+        # Calculate health ratio
+        ratio = self.hp / self.max_hp
         pygame.draw.rect(surface, "red", (self.x, self.y, self.w, self.h))
         pygame.draw.rect(surface, "green", (self.x, self.y, self.w * ratio, self.h))
 
@@ -59,11 +61,9 @@ clock = pygame.time.Clock()
 player1 = Player(100, 400)
 player2 = Player(400, 400)
 floor = Floor(0, 550, 800, 50)  # Creating a floor rectangle
-health_bar1 = HealthBar(10,10, 300, 40, 100)
-
+health_bar1 = HealthBar(10, 10, 300, 40, 100)
 
 projectiles = []  # List to store active projectiles
-
 
 running = True
 while running:
@@ -74,10 +74,10 @@ while running:
     # Clear the screen
     screen.fill((0, 0, 0))
 
-    #healthbars
+    # Health bars
     health_bar1.draw(screen)
 
-    #physics
+    # Physics
     # Check for collisions and apply gravity
     if player1.current_position[1] + 40 <= floor.rect.y:  # Adjusted for character's height
         player1.current_position = (player1.current_position[0], floor.rect.y - 40)  # Set character on top of the floor
@@ -90,58 +90,45 @@ while running:
         player2.move(0, 2)  # Apply gravity to player2
 
     # Move and draw player objects
-    #if keypress  = w:
-        #move player1 up in the y axis
-    #if keypress = d:
-        #move player1 to the right    
     keys = pygame.key.get_pressed()
-    # Move player1 up in the y-axis
 
-    #player 1 movement
+    # Player 1 movement
     if keys[pygame.K_w]:
         player1.move(0, -20)
     if keys[pygame.K_d]:  # Move player1 to the right
         player1.move(2, 0)  # Positive dx value moves character to the right
-    # if keys[pygame.K_s]:  # Move player1 up in the y-axis
-    #     player1.move(0, 2)  # Negative dy value moves character upward
-    if keys[pygame.K_a]:  # Move player1 to the right
-        player1.move(-2, 0)  # Positive dx value moves character to the right
+    if keys[pygame.K_a]:  # Move player1 to the left
+        player1.move(-2, 0)  # Negative dx value moves character to the left
 
+    # Projectile throw for player1
     if keys[pygame.K_r]:
-        #might need to + the pixels of the player to current positions
-        throw = Projectile(player1.current_position[0],player1.current_position[1], 2)
+        throw = Projectile(player1.current_position[0], player1.current_position[1], 2)
         projectiles.append(throw)  # Add the new projectile to the list
         print("projectile thrown")
 
+    # Remove collided projectiles
     projectiles_to_remove = []  # List to store projectiles that should be removed
-
-    # Move and draw projectiles
     for projectile in projectiles:
-        projectile.move(projectile.velocity, 0, 2)  # Move projectile horizontally
+        projectile.move(projectile.velocity, 0)  # Move projectile horizontally
         pygame.draw.circle(screen, (255, 255, 0), projectile.current_position, 10)  # Draw the projectile
         
-        # Check for collision with player2
-        if (player2.current_position[0] - 20 == projectile.current_position[0]) and \
-           (player2.current_position[1] == projectile.current_position[1]):
+        # Check for collision with player2 using bounding boxes
+        if projectile.rect.colliderect(player2_rect):
             projectiles_to_remove.append(projectile)
             print(health_bar1.hp)
             health_bar1.hp = health_bar1.hp - 1
             print(health_bar1.hp)
 
-        # Remove collided projectiles
     for projectile in projectiles_to_remove:
         projectiles.remove(projectile)
 
-    #player 2 movement
-    if keys[pygame.K_UP]:  # Move player1 up in the y-axis
+    # Player 2 movement
+    if keys[pygame.K_UP]:  # Move player2 up in the y-axis
         player2.move(0, -20)  # Negative dy value moves character upward
-    if keys[pygame.K_RIGHT]:  # Move player1 to the right
+    if keys[pygame.K_RIGHT]:  # Move player2 to the right
         player2.move(2, 0)  # Positive dx value moves character to the right
-    # if keys[pygame.K_DOWN]:  # Move player1 up in the y-axis
-    #     player2.move(0, 2)  # Negative dy value moves character upward
-    if keys[pygame.K_LEFT]:  # Move player1 to the right
-        player2.move(-2, 0)  # Positive dx value moves character to the right
-
+    if keys[pygame.K_LEFT]:  # Move player2 to the left
+        player2.move(-2, 0)  # Negative dx value moves character to the left
 
     # Draw floor
     floor.draw(screen)
@@ -152,24 +139,7 @@ while running:
     pygame.draw.rect(screen, (255, 0, 0), player1_rect)
     pygame.draw.rect(screen, (0, 0, 255), player2_rect)
 
-    
-
-
-
     pygame.display.flip()
     clock.tick(60)
 
 pygame.quit()
-
-
-#physics temp
-    # Move and draw player objects
-    #if keypress = d:
-        #move player1 to the right
-
-    #create a floor
-    #if position =< floor:
-        #position = floor
-            #or
-        #gravity = 0
-
